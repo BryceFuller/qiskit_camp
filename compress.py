f017393754f70e3781e632064e0babf2460a5dc6
Dan Padilha <dan.padilha@qxbranch.com>
2019-02-28 06:46:10 +1100

    Add a swap test to build_compression_model.

--- compress.py
+++ compress.py
@@ -1,10 +1,11 @@
 # Python
 import logging
+from copy import deepcopy
 
 # Local
 
 # External
-from qiskit import QuantumCircuit, ClassicalRegister
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 
 
 def build_compression_model(orig_circuit):
@@ -24,7 +25,7 @@ def build_compression_model(orig_circuit):
     model_circuit = QuantumCircuit(*new_registers)
 
     # TODO: Build the model's compression circuit here.
-    # model_circuit.h(*new_registers)
+    # model_circuit.x(*new_registers)
 
     # Append the two circuits together.
     top_circuit = orig_circuit + model_circuit
@@ -32,18 +33,21 @@ def build_compression_model(orig_circuit):
     # Synchronisation barrier just so we know where the original circuits ends.
     top_circuit.barrier()
 
-    # Perform a SWAP test here on the quantum registers.
-    # TODO: This should be done outside of this method.
+    # Performs a swap test between the compression model and original circuit.
+    # ------------------------------------------------------------------------
+    # Firstly, create an ancillary in superposition to store the swap test result.
+    swap_tester = QuantumRegister(1, 'swap_tester')
+    top_circuit.add_register(swap_tester)
+    top_circuit.h(swap_tester)
+
+    # Next, we perform controlled SWAPs using the swap tester.
     for orig_reg, model_reg in zip(orig_circuit.qregs, model_circuit.qregs):
-        top_circuit.swap(orig_reg, model_reg)
+        top_circuit.cswap(swap_tester, orig_reg, model_reg)
 
-        # Build new classical measurement registers.
-        measure_reg_orig  = ClassicalRegister(orig_reg.size, '{}_measure'.format(orig_reg.name))
-        measure_reg_model = ClassicalRegister(orig_reg.size, '{}_measure'.format(model_reg.name))
-        top_circuit.add_register(measure_reg_orig, measure_reg_model)
-
-        # Measure the swapped registers.
-        top_circuit.measure(orig_reg, measure_reg_orig)
-        top_circuit.measure(model_reg, measure_reg_model)
+    # Finally, we re-interfere the branches and measure the swap tester.
+    top_circuit.h(swap_tester)
+    swap_test_result = ClassicalRegister(1, 'swap_test_result')
+    top_circuit.add_register(swap_test_result)
+    top_circuit.measure(swap_tester, swap_test_result)
 
     return top_circuit
