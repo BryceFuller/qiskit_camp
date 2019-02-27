1afbaa84bd40a9ed138b0579a8ea0bb5573b6f5c
Dan Padilha <dan.padilha@qxbranch.com>
2019-02-28 05:48:19 +1100

    Add build_compression_model().

--- compress.py
+++ compress.py
@@ -1,4 +1,5 @@
 # Python
+import logging
 
 # Local
 
@@ -7,6 +8,32 @@ from qiskit import QuantumCircuit
 from qiskit import execute
 
 
-class CompressTest:
-    pass
+def build_compression_model(circuit):
+    """ Given a circuit, builds a parametrised sub-circuit that runs in
+    parallel with and approximately models (with compression) the original
+    circuit.
 
+    Return:
+        QuantumCircuit: new combined circuit consisting of the original circuit
+        and the model circuit.
+    """
+
+    # We assume that all QUANTUM registers of the original circuit are to be
+    # modelled.
+    new_registers = [reg.__class__(reg.size, '{}_model'.format(reg.name))
+                     for reg in circuit.qregs]
+    model_circuit = QuantumCircuit(*new_registers)
+
+    # TODO: Build the model's compression circuit here.
+
+    # Append the two circuits together.
+    top_circuit = circuit + model_circuit
+
+    # Synchronisation barrier just so we know where the original circuits ends.
+    top_circuit.barrier()
+
+    # Perform a SWAP test here.
+    # TODO: This should be done outside of this method.
+    # top_circuit.swap(tuple(circuit.qregs), tuple(new_registers))
+
+    return top_circuit
