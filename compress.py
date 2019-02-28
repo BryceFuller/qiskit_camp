ec50ecd063f7aa63420d7c3a86e38ad190af0bbc
Dan Padilha <dan.padilha@qxbranch.com>
2019-02-28 14:07:06 +1100

    Working optimisation loop with model parameters.

--- compress.py
+++ compress.py
@@ -6,11 +6,49 @@ from functools import partial
 # Local
 
 # External
-from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, execute
-from qiskit.aqua.components.optimizers import *
+import numpy as np
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, execute, Aer
+from qiskit.aqua.components.optimizers import SPSA
 
 
-def build_compression_model(registers, model_parameters=None):
+def build_model_from_parameters(circuit, theta):
+
+    # XXX: We reset the random seed here because we want the gate rotations to remain consistent
+    seed = 123456789
+    np.random.seed(seed)
+
+    logging.critical("Building new model approximation with parameters {}".format(theta.T))
+
+    layers, num_qubits = theta.shape
+    random_gates = [circuit.rx, circuit.ry, circuit.rz]
+
+    # Combine all the qubits in the circuit into one flat list
+    all_qubits = [(reg, qubit) for register in circuit.qregs for reg, qubit in register]
+    assert len(all_qubits) == num_qubits, \
+        "There are {} qubits but expected same as parameter's shape ({})!".format(len(all_qubits), num_qubits)
+
+    for l in range(layers):
+
+        for n, (reg, qubit_idx) in enumerate(all_qubits):
+            # Add layer of (pseudo)random single qubit rotations
+            random_gate = np.random.choice(random_gates)
+            random_gate(theta[l][n], reg[qubit_idx])
+            logging.critical(random_gate)
+
+        circuit.barrier()
+
+        # Pattern with 2-qubit gates
+        # change the step size in range to affect density of cx layer
+        for n in range(0, num_qubits, 2):
+            if (n + 1) + (l % 2) < num_qubits:  # Out-of-bounds check
+                reg_1, qubit_idx_1 = all_qubits[n + (l % 2)]
+                reg_2, qubit_idx_2 = all_qubits[(n + 1) + (l % 2)]
+                circuit.cx(reg_1[qubit_idx_1], reg_2[qubit_idx_2])
+
+        circuit.barrier()
+
+
+def build_compression_model(registers, model_parameters):
     """ Given a set of input registers, builds a parametrised model of random
     gate operations which seek to approximate some other (equally-sized) target
     circuit.
@@ -20,16 +58,19 @@ def build_compression_model(registers, model_parameters=None):
         registers.
     """
     model_circuit = QuantumCircuit(*registers)
-    logging.critical("Building new model approximation with parameters {}".format(model_parameters))
+
+    # Reshape model parameters so they make sense
+    n_params = int(len(model_parameters) / model_circuit.width())
+    model_parameters = np.array(model_parameters).reshape((n_params, model_circuit.width()))
 
     # TODO: Store some internal parametrization that can be optimised over.
     # TODO: Create random set of gate operations based on parametrization.
-    # build_model(model_parameters)
+    build_model_from_parameters(model_circuit, model_parameters)
 
     return model_circuit
 
 
-def swap_test_with_compression_model(target_circuit, model_parameters=None):
+def swap_test_with_compression_model(target_circuit, model_parameters):
     """ Given a circuit, builds a parametrised sub-circuit that runs in
     parallel with and approximately models (with compression) the original
     circuit.
@@ -75,7 +116,7 @@ def swap_test_with_compression_model(target_circuit, model_parameters=None):
     return top_circuit
 
 
-def compute_approximation_fidelity(target_circuit, backend='qasm_simulator', n_shots=1000, model_parameters=None):
+def compute_approximation_fidelity(target_circuit, backend='qasm_simulator', n_shots=1000, model_parameters=[]):
     """ Performs a set of runs on the target circuit and an approximation model
     to compute the approximation's fidelity.
 
@@ -92,6 +133,8 @@ def compute_approximation_fidelity(target_circuit, backend='qasm_simulator', n_s
     # Return a measurement of fidelity
     result_counts = job.result().get_counts(final_circuit)
     fidelity = result_counts.get('1', result_counts.get('0')) / sum(result_counts.values())
+
+    logging.critical("Computed fidelity: {}".format(fidelity))
     return fidelity
 
 
@@ -121,8 +164,8 @@ if __name__ == "__main__":
     circuit.h(q0)
 
     # Configuration
-    n_shots = 100
-    n_trials = 100
+    n_shots  = 100
+    n_trials = 1000
     n_layers = 5
     n_params = circuit.width() * n_layers
 
@@ -131,17 +174,19 @@ if __name__ == "__main__":
     variable_bounds = [variable_bounds_single] * n_params
     initial_point = np.random.uniform(low=variable_bounds_single[0],
                                       high=variable_bounds_single[1],
-                                      size=(n_params,))
+                                      size=(n_params,)).tolist()
 
     # Partially define the objective function
-    objective_function = partial(compute_approximation_fidelity, circuit, "qasm_simulator", n_shots)
+    maximisation_function = partial(compute_approximation_fidelity, circuit, "qasm_simulator", n_shots)
+    minimization_function = lambda params: -maximisation_function(params)
 
     # Call the optimiser
     optimizer = SPSA(max_trials=n_trials)
-    result = optimizer.optimize(n_params, objective_function,
+    result = optimizer.optimize(n_params, minimization_function,
                                 variable_bounds=variable_bounds, initial_point=initial_point)
 
-    logging.critical(result)
+    last_params, last_score, _ = result
+    logging.critical("FINAL PARAMETERS: {} --> SCORE: {}".format(last_params, -last_score))
 
-    #Transfer data into dict
-    #TODO calculate compiled depth (for simulator this is always 2*l)
+    # Transfer data into dict
+    # TODO calculate compiled depth (for simulator this is always 2*l)
